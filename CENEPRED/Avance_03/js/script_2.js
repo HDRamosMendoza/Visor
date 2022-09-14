27f25b038dbe453a3365b5941081cd06a8ec996e
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2022-09-13 21:44:12 -0500

    Se solucionno el problema que al darle clic en el nombre del grupo del menu vertical se reliza una consulta

--- CENEPRED/Avance_03/js/script_2.js
+++ CENEPRED/Avance_03/js/script_2.js
@@ -414,14 +414,16 @@ require([
     let _class = function(name) { return document.getElementsByClassName(name); };
 	let tabPanes = _class("tab-header")[0].getElementsByTagName("div");	
 	for(let i=0;i<tabPanes.length;i++) {
-		tabPanes[i].addEventListener("click", function() {		
-			_class("tab-header")[0].getElementsByClassName("active")[0].classList.remove("active");
-			tabPanes[i].classList.add("active");
-			/*_class("tab-indicator")[0].style.top = `calc(80px + ${i*50}px)`;*/			
-			_class("tab-content")[0].getElementsByClassName("active")[0].classList.remove("active");
-			_class("tab-content")[0].getElementsByTagName("div")[i].classList.add("active");
-            featureTable.destroy();
-            _featureTable(tabPanes[i].getAttribute("data-url"), tabPanes[i].getAttribute("data-objectid"), JSON.parse(tabPanes[i].getAttribute("data-fields")));
+		tabPanes[i].addEventListener("click", function() {
+            if(typeof tabPanes[i].getAttribute("data-url") == "string") { /* Se filtra solo para los que tenga URL */
+                _class("tab-header")[0].getElementsByClassName("active")[0].classList.remove("active");
+                tabPanes[i].classList.add("active");
+                /*_class("tab-indicator")[0].style.top = `calc(80px + ${i*50}px)`;*/			
+                _class("tab-content")[0].getElementsByClassName("active")[0].classList.remove("active");
+                _class("tab-content")[0].getElementsByTagName("div")[i].classList.add("active");
+                featureTable.destroy();
+                _featureTable(tabPanes[i].getAttribute("data-url"), tabPanes[i].getAttribute("data-objectid"), JSON.parse(tabPanes[i].getAttribute("data-fields")));
+            }
 		});
 	}    
     map.on("load", () => { _graphicPie(); });
