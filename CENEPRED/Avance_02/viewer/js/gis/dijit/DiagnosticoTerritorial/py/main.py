fe9712631e0ec097b8f6e007143e125b174c8449
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2022-10-02 23:03:56 -0500

    EXPORTA DATA. Se implementa para descargar GDB

--- CENEPRED/Avance_02/viewer/js/gis/dijit/DiagnosticoTerritorial/py/main.py
+++ CENEPRED/Avance_02/viewer/js/gis/dijit/DiagnosticoTerritorial/py/main.py
@@ -9,13 +9,11 @@ import json
 
 # Default Folder
 scratch_Folder = arcpy.env.scratchFolder
-print(scratch_Folder)
 
 # Default GDB
 scratch_GDB = arcpy.env.scratchGDB
-print(scratch_GDB)
 
-# Workspace
+# Workspace - PATH .SDE
 arcpy.env.workspace = os.path.join(r"D:\RepositorioGitHub\Visor\CENEPRED\Avance_02\viewer\js\gis\dijit\DiagnosticoTerritorial\py\sigrid.gdb")
 
 # Spatial Reference
@@ -41,22 +39,25 @@ nameFileSHP_Zip = str("SIGRID_SHP_ZIP_" + time_file)
 nameFileGDB = str("SIGRID_GDB_" + time_file)
 nameFileGDB_Zip = str("SIGRID_GDB_ZIP_" + time_file)
 
-# Param list layer
-#geoLayer = arcpy.GetParameterAsText(0)
-geoLayer = 'ZRMN,EVAR,planes_PPRRD'
-# Param polygon instersect
-#geojson_polygon = arcpy.GetParameterAsText(1)
-#geoFormat = arcpy.GetParameterAsText(2)
-geoFormat = "SHP"
-# Param example polygon
-geojson_polygon ='''
+# Lista de capas (cadena separadas por comas)
+geoLayer = arcpy.GetParameterAsText(0)
+# Poligono de intersección
+geojson_polygon = arcpy.GetParameterAsText(1)
+# Formato a descargar
+geoFormat = arcpy.GetParameterAsText(2)
+
+#geoLayer = 'ZRMN,EVAR,planes_PPRRD'
+#geoFormat = "GDB" || "SHP" || "GDB" || "PRUEBA"
+'''
+geojson_polygon =
                 { 
                     "type": "Polygon", 
                     "coordinates": [
                         [[-79.8486328125,-7.1663003819031825],[-78.22265625,-8.993600464280018],[-75.52001953125,-6.271618064314864],[-79.16748046874999,-5.615985819155327],[-79.8486328125,-7.1663003819031825]]
                     ],
                     "spatialReference" : { "wkid" : 4326 }
-                }'''
+                }
+'''
 arcpy.AddMessage("Parametro 1: " + geoLayer)
 arcpy.AddMessage("Parametro 2: " + geoFormat)
 arcpy.AddMessage("Parametro 3: " + geojson_polygon)
@@ -111,11 +112,9 @@ if __name__ == '__main__':
             for f in files:
                 if f.endswith(".kmz"):
                     zfile.write(os.path.join(scratch_Folder,nameFileKMZ,f))
-
             zfile.close()
             # Response KMZ
             arcpy.SetParameterAsText(4, _pathZip)
-            print(_pathZip)
         
         # Download SHP
         if(geoFormat == "SHP"):
@@ -144,19 +143,46 @@ if __name__ == '__main__':
             for f in files:
                 if f.endswith("shp") or f.endswith("dbf") or f.endswith("shx") or f.endswith("cpg") or f.endswith("prj") or f.endswith("sbn") or f.endswith("sbx") or f.endswith("xml"):
                     zfile.write(os.path.join(scratch_Folder,nameFileSHP,f))
-
             zfile.close()
             # Response KMZ
             arcpy.SetParameterAsText(4, _pathZip)
         
         # Download GDB
         if(geoFormat == "GDB"):
-            print("GDB")
+            if not arcpy.Exists(nameFileGDB):
+                os.mkdir(os.path.join(scratch_Folder,nameFileGDB))
 
-
+            arcpy.CreateFileGDB_management(os.path.join(scratch_Folder,nameFileGDB), "SIGRID_GDB.gdb")
+            _GDB = os.path.join(scratch_Folder,nameFileGDB,"SIGRID_GDB.gdb")
+            print(_GDB)
+            for layer in item:
+                if(arcpy.Exists(layer)):
+                    # Add name GDB
+                    layer_temp = 'lyrGDB' + layer + time_file
+                    #Se crear un Layer para su uso
+                    arcpy.MakeFeatureLayer_management(layer, layer_temp)
+                    layer = layer.replace(".", "")
+                    # Nombre del GDB
+                    name_GDB = "SIGRID_CENEPRED_GDB_" + layer + "_" + time_file
+                    # Selección por localización
+                    arcpy.SelectLayerByLocation_management(layer_temp,"INTERSECT", polygon)
+                    # Ruta de destino de la conversion de un GDB
+                    arcpy.CopyFeatures_management(layer_temp, os.path.join(_GDB, name_GDB))
+                else:
+                    print("Not exist: {}".format(layer))
+            
+            _pathZip = os.path.join(scratch_Folder,nameFileGDB,nameFileGDB_Zip + ".zip")
+            zfile = zipfile.ZipFile(_pathZip, "w", zipfile.ZIP_DEFLATED)
+            for root, dirs, files in os.walk(os.path.join(scratch_Folder,nameFileGDB)):
+                if root == _GDB:
+                    for f in files:
+                        zfile.write(os.path.join(root, f))
+            zfile.close()
+            # Response GDB
+            arcpy.SetParameterAsText(4, _pathZip)
 '''
-# Nota: https://diluvium.colorado.edu/arcgis/portalhelp/en/notebook/latest/python/linux/listfeatureclasses.htm
-    Te lista los FEATURE sin embargo no te valida si existe en la base de datos o no. 
+Nota: 
+    Te lista los FEATURE sin embargo no te valida si existe en la base de datos o no.
     En ocasiones aparece en la GDB sin embargo no existe. Para ello utiliza ARCPY.EXITS
-
+    https://diluvium.colorado.edu/arcgis/portalhelp/en/notebook/latest/python/linux/listfeatureclasses.htm
 '''
