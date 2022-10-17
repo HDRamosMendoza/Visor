5f6622ac56647fb82dc3c71bbe52983aa4f80cd3
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2022-10-17 14:32:17 -0500

    EXTRAC DATA. Obtiene solo el nombre de la tabla

---
+++ CENEPRED/Avance_02/viewer/js/gis/dijit/DiagnosticoTerritorial/py/prueba.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import random
+import time
+import json
+
+def _nameAlone(_name):
+    _name = _name.replace(".", "")
+    _name = _name.replace("/", "")
+    if('/' in _name):
+        return _name[_name.rfind('/')+1:] 
+    else:
+        return _name
+
+_name = "lyrSHPbdcenepred.cartografia_p/eligros.Hidrom_Oceanograficos/bdcenepred.cartografia_peligros.Inventario_inundacion_217102022_973121794"
+
+if __name__ == '__main__':
+    _nameAlone(_name)
